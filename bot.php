6b3ba5e3a131dd39f8b125f3f9bb4ea64f6a47a8
developper777 <91107169+developper777@users.noreply.github.com>
2021-12-23 06:03:39 -0400

    Update bot.php

--- bot.php
+++ bot.php
@@ -56,7 +56,7 @@ switch ($tlg->Text ()){
 
 	$tlg->sendMessage ([
 		'chat_id' => $tlg->ChatID (),
-		'text' => 'Bot original @DARKNETSSHBOTt por @DARKNETSSH'
+		'text' => 'Bot original @DARKNETSSHBOT por @DARKNETSSH'
 	]);
 
 	break;
